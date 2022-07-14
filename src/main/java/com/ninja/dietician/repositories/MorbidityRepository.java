a13b652165478eaf6c7a71c79ac9166a807238f0
Asmitha <asmitharaj@gmail.com>
2022-07-14 12:50:20 -0400

    added json ignore and filter by infoType

--- src/main/java/com/ninja/dietician/repositories/MorbidityRepository.java
+++ src/main/java/com/ninja/dietician/repositories/MorbidityRepository.java
@@ -1,7 +1,5 @@
 package com.ninja.dietician.repositories;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +8,6 @@ import org.springframework.stereotype.Repository;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.ninja.dietician.entity.Morbidity;
 
 @Repository
@@ -20,92 +16,36 @@ public class MorbidityRepository {
 	@Autowired
 	private DynamoDBMapper mapper;
 
+	public List<Morbidity> findAll(DynamoDBScanExpression scanExpression) {
+		return mapper.scan(Morbidity.class, scanExpression);
+	}
+
 	public void addMorbidity(Morbidity user) {
 		mapper.save(user);
 	}
 
-	@SuppressWarnings("unchecked")
-	public List getMorbidity() {
-		List<Morbidity> list = new ArrayList();
-		list = mapper.scan(Morbidity.class, new DynamoDBScanExpression());
-		return list;
-		//return (PaginatedScanList<T>) mapper.scan(Morbidity.class, new DynamoDBScanExpression());
-	}
-
-	public void deleteMorbidities(String morbidityName, String morbidityTestId) {
-		
-
-		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
-		eav1.put(":v1", new AttributeValue().withS(morbidityName));
-		eav1.put(":v2", new AttributeValue().withS(morbidityTestId));
-
-		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-				// Filter Expression
-				.withFilterExpression("MorbidityName = :v1 and MorbidityTestId = :v2")
-				.withExpressionAttributeValues(eav1);
-
-		List<Morbidity> morbidityList = mapper.scan(Morbidity.class, scanExpression);
-		mapper.delete(morbidityList.get(0));
+	public void deleteMorbidities(DynamoDBScanExpression scanExpression) {
+		mapper.delete(mapper.scan(Morbidity.class, scanExpression).get(0));
 
 	}
 
-	public Morbidity updateMorbidities(String morbidityName, String morbidityTestId, Morbidity morbidity) {
-
-		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
-		eav1.put(":v1", new AttributeValue().withS(morbidityName));
-		eav1.put(":v2", new AttributeValue().withS(morbidityTestId));
-
-		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-				// Filter Expression
-				.withFilterExpression("MorbidityName = :v1 and MorbidityTestId = :v2")
-				.withExpressionAttributeValues(eav1);
-
-		List<Morbidity> morbidityList = mapper.scan(Morbidity.class, scanExpression);
-
-		morbidityList.forEach(mor -> {
-			mor.setMorbidityTestName(morbidity.getMorbidityTestName());
-			mor.setMorbidityTestUnit(morbidity.getMorbidityTestUnit());
-			mor.setMorbidityTestMarkerRef(morbidity.getMorbidityTestMarkerRef());
-
-			DynamoDBMapperConfig dynamoDBMapperConfig = new DynamoDBMapperConfig.Builder()
-					.withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
-					.withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE).build();
-			mapper.save(mor, dynamoDBMapperConfig);
-
-		});
-
-		return morbidityList.get(0);
+	public Morbidity updateMorbidities(DynamoDBMapperConfig dynamoDBMapperConfig, Morbidity morbidity) {
+		return morbidity;
 
 	}
 
-	public List<Morbidity> getMorbiditybyName(String morbidityName) {
-		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
-		eav1.put(":v1", new AttributeValue().withS(morbidityName));
-		
-
-		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-				// Filter Expression
-				.withFilterExpression("MorbidityName = :v1 ")
-				.withExpressionAttributeValues(eav1);
-
-		List<Morbidity> morbidityList = mapper.scan(Morbidity.class, scanExpression);
-		
-		return morbidityList;
+	public List<Morbidity> getMorbidityList(DynamoDBScanExpression scanExpression) {
+		return mapper.scan(Morbidity.class, scanExpression);
 	}
 
-	public List<Morbidity> getMorbiditybyMorbidityTestId(String morbidityTestId) {
-		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
-		eav1.put(":v1", new AttributeValue().withS(morbidityTestId));
-		
+	public List<Morbidity> getMorbiditybyName(DynamoDBScanExpression scanExpression) {
+		return mapper.scan(Morbidity.class, scanExpression);
 
-		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
-				// Filter Expression
-				.withFilterExpression(" MorbidityTestId = :v1")
-				.withExpressionAttributeValues(eav1);
+	}
 
-		List<Morbidity> morbidityList = mapper.scan(Morbidity.class, scanExpression);
-		
-		return morbidityList;
+	public List<Morbidity> getMorbiditybyMorbidityTestId(DynamoDBScanExpression scanExpression) {
+		return mapper.scan(Morbidity.class, scanExpression);
+
 	}
 
 }
--- src/main/java/com/ninja/dietician/service/serviceImpl/MorbidityServiceImpl.java
+++ src/main/java/com/ninja/dietician/service/serviceImpl/MorbidityServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ninja.dietician.service.serviceImpl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedScanList;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.ninja.dietician.entity.Morbidity;
 import com.ninja.dietician.repositories.MorbidityRepository;
 import com.ninja.dietician.service.MorbidityService;
@@ -16,10 +19,19 @@ public class MorbidityServiceImpl implements MorbidityService {
 
 	@Autowired
 	MorbidityRepository morbiditySaveRepository;
+	
+	private String infoType = "Morbidity";
 
 	@Override
 	public List<Morbidity> getMorbidities() {
-		return morbiditySaveRepository.getMorbidity();
+		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
+		eav1.put(":v1", new AttributeValue().withS(infoType));
+
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				// Filter Expression
+				.withFilterExpression("InfoType = :v1 ").withExpressionAttributeValues(eav1);
+
+		return morbiditySaveRepository.findAll(scanExpression);
 	}
 
 	@Override
@@ -38,26 +50,77 @@ public class MorbidityServiceImpl implements MorbidityService {
 
 	@Override
 	public String deleteMorbidities(String morbidityName, String morbidityTestId) {
-	
-		this.morbiditySaveRepository.deleteMorbidities(morbidityName,morbidityTestId);
+
+		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
+		eav1.put(":v1", new AttributeValue().withS(morbidityName));
+		eav1.put(":v2", new AttributeValue().withS(morbidityTestId));
+
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				// Filter Expression
+				.withFilterExpression("MorbidityName = :v1 and MorbidityTestId = :v2")
+				.withExpressionAttributeValues(eav1);
+
+		this.morbiditySaveRepository.deleteMorbidities(scanExpression);
 		return "A record is deleted successfully";
 	}
 
 	@Override
 	public Morbidity updateMorbidities(String morbidityName, String morbidityTestId, Morbidity morbidity) {
-		return this.morbiditySaveRepository.updateMorbidities(morbidityName, morbidityTestId, morbidity);
+
+		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
+		eav1.put(":v1", new AttributeValue().withS(morbidityName));
+		eav1.put(":v2", new AttributeValue().withS(morbidityTestId));
+
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				// Filter Expression
+				.withFilterExpression("MorbidityName = :v1 and MorbidityTestId = :v2")
+				.withExpressionAttributeValues(eav1);
+
+		List<Morbidity> morbidityList = this.morbiditySaveRepository.getMorbidityList(scanExpression);
+
+		morbidityList.forEach(mor -> {
+			mor.setMorbidityTestName(morbidity.getMorbidityTestName());
+			mor.setMorbidityTestUnit(morbidity.getMorbidityTestUnit());
+			mor.setMorbidityTestMarkerRef(morbidity.getMorbidityTestMarkerRef());
+
+			DynamoDBMapperConfig dynamoDBMapperConfig = new DynamoDBMapperConfig.Builder()
+					.withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
+					.withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE).build();
+
+			this.morbiditySaveRepository.updateMorbidities(dynamoDBMapperConfig, mor);
+
+		});
+
+		return morbidityList.get(0);
 	}
 
 	@Override
 	public List<Morbidity> getMorbiditybyName(String morbidityName) {
-		// TODO Auto-generated method stub
-		return this.morbiditySaveRepository.getMorbiditybyName(morbidityName);
+
+		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
+		eav1.put(":v1", new AttributeValue().withS(morbidityName));
+		eav1.put(":v2", new AttributeValue().withS(infoType));
+
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				// Filter Expression
+				.withFilterExpression("MorbidityName = :v1 and InfoType = :v2")
+				.withExpressionAttributeValues(eav1);
+
+		return this.morbiditySaveRepository.getMorbiditybyName(scanExpression);
 	}
 
 	@Override
-	public List<Morbidity>  getMorbiditybyMorbidityTestId(String morbidityTestId) {
-		// TODO Auto-generated method stub
-		return this.morbiditySaveRepository.getMorbiditybyMorbidityTestId(morbidityTestId);
+	public List<Morbidity> getMorbiditybyMorbidityTestId(String morbidityTestId) {
+
+		HashMap<String, AttributeValue> eav1 = new HashMap<String, AttributeValue>();
+		eav1.put(":v1", new AttributeValue().withS(morbidityTestId));
+		eav1.put(":v2", new AttributeValue().withS(infoType));
+
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+				// Filter Expression
+				.withFilterExpression(" MorbidityTestId = :v1 and InfoType = :v2")
+				.withExpressionAttributeValues(eav1);
+		return this.morbiditySaveRepository.getMorbiditybyMorbidityTestId(scanExpression);
 	}
 
 }
