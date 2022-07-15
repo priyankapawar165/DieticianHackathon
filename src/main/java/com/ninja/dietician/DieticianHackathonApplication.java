a12ef5421f29767c6e6142224e0b461f975da5d0
vidsdn <vidsdn@gmail.com>
2022-07-14 21:05:51 -0500

    API documentation for Phase 3

--- src/main/java/com/ninja/dietician/DieticianHackathonApplication.java
+++ src/main/java/com/ninja/dietician/DieticianHackathonApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,6 +24,7 @@ public class DieticianHackathonApplication {
 	@Bean
     public Docket dieticianApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+        		.apiInfo(getApiInformation())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ninja.dietician"))
                 .paths(PathSelectors.any())
@@ -28,4 +32,16 @@ public class DieticianHackathonApplication {
 
 
     }
+	
+	private ApiInfo getApiInformation(){
+		
+		Contact contact = new Contact("Team - Ninja Turtles ","https://www.numpyninja.com/","");
+		
+		return new ApiInfoBuilder()
+	              .title("API Documentation For Dietician Hackathon 2022")
+	              .description("This is the Dietician API created using Spring Boot and DynamoDB")
+	              .contact(contact)
+	              .version("1.0.0")
+	              .build();
+	}
 }
